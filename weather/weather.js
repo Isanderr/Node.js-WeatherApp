e3d8d0270a7ba670e3ddd7bae578e145ac9d9563
Łukasz Cendrowski <isander@wp.pl>
2018-10-21 17:09:07 +0200

    Add files via upload

---
+++ weather/weather.js
@@ -0,0 +1,20 @@
+const request = require('request');
+
+var getWeather = (lat, lng, callback) => {
+    request({
+        url: `https://api.darksky.net/forecast/4a76fb8cbe533412212d5461ffaed286/${lat},${lng}`,
+        json: true
+    }, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+            callback(undefined, {
+                temperature: body.currently.temperature,
+                apparentTemperature: body.currently.apparentTemperature
+            });
+        } else {
+            callback('Unable to fetch weather.');
+        }
+    });
+};
+
+
+module.exports.getWeather = getWeather;
